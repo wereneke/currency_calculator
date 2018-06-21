15a528eae9ed86d6e374aa2cf1b93a74abb7e2db
wereneke <weronikad752@gmail.com>
2018-06-21 16:29:06 +0200

    create project

---
+++ src/main/java/com/codecool/wera/currency_calculator/CurrencyCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.wera.currency_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCalculatorApplication.class, args);
+	}
+}
