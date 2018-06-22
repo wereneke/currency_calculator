744d7754e7289ba437da4d12124bd033e5c8789f
wereneke <weronikad752@gmail.com>
2018-06-22 17:42:17 +0200

    configure database

---
+++ src/main/java/com/codecool/wera/currency_calculator/configuration/CurrencyMVCConfig.java
@@ -0,0 +1,4 @@
+package com.codecool.wera.currency_calculator.configuration;
+
+public class CurrencyMVCConfig {
+}
---
+++ src/main/java/com/codecool/wera/currency_calculator/logging/CurrencyRequestInterceptor.java
@@ -0,0 +1,4 @@
+package com.codecool.wera.currency_calculator.logging;
+
+public class CurrencyRequestInterceptor {
+}
